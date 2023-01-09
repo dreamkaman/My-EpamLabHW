32e42dccfa1852cf9c66f1fc836381683e9640b1
Dreamkaman <riabushko@gmail.com>
2023-01-09 22:21:40 +0200

    Added small corrections

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from 'react';
 
 import Header from './components/Header/Header';
 import Courses from './components/Courses/Courses';
+import CreateCourse from 'components/CreateCourse';
 
 import { Context } from './Context';
 
 import * as db from 'helpers/mockedDataBase';
-import CreateCourse from 'components/CreateCourse';
 
 const initialCoursesSet = db.mockedCoursesList;
 const authorsInitial = db.mockedAuthorsList;
